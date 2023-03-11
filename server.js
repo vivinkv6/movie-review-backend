91f2d70992dfeeccae2807307fdc4aeea4333d23
VIVIN KV <102971812+vivinkv6@users.noreply.github.com>
2023-03-11 14:11:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const cors = require('cors');
 
 const corsOptions = {
-  origin: 'https://movie-review.cyclic.app/api',
+  origin: 'https://movie-review.cyclic.app',
   optionsSuccessStatus: 200
 }
 
