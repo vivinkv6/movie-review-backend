f01dbaa3e06472cafbf2c3e6505bad671174612c
vivinkv6 <vivinkv0@gmail.com>
2023-03-11 11:12:55 +0530

    ready to deploy

--- server.js
+++ server.js
@@ -3,7 +3,16 @@ const express = require("express");
 const movieReview = require("./Routes/review");
 const mongoose = require("mongoose");
 const app = express();
+const cors = require('cors');
 
+const corsOptions = {
+  origin: 'https://movie-review.cyclic.app',
+  optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions));
+
+app.use(cors());
 app.use(express.json());
 app.use("/api", movieReview);
 
